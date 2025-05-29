a773f5e11ecc3cc5b54130266ce1066d6e7a7118
Marc Santacana <marcsh05@gmail.com>
2025-05-29 19:04:58 +0000

    feat: afegit menú principal

--- ENIGMA C++/main.cpp
+++ ENIGMA C++/main.cpp
@@ -3,17 +3,54 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+    std::cout << R"(
+    ________    __    __    ______     ______     __       __     ______
+   |        \  |  \  |  \  |      \   /      \   |  \     /  \   /      \ 
+   | $$$$$$$$  | $$\ | $$   \$$$$$$  |  $$$$$$\  | $$\   /  $$  |  $$$$$$\
+   | $$__      | $$$\| $$    | $$    | $$ __\$$  | $$$\ /  $$$  | $$__| $$
+   | $$  \     | $$$$\ $$    | $$    | $$|    \  | $$$$\  $$$$  | $$    $$
+   | $$$$$     | $$\$$ $$    | $$    | $$ \$$$$  | $$\$$ $$ $$  | $$$$$$$$
+   | $$_____   | $$ \$$$$   _| $$_   | $$__| $$  | $$ \$$$| $$  | $$  | $$
+   | $$     \  | $$  \$$$  |   $$ \   \$$    $$  | $$  \$ | $$  | $$  | $$
+    \$$$$$$$$   \$$   \$$   \$$$$$$    \$$$$$$    \$$      \$$   \$$   \$$
+)" << std::endl;
+
+    std::cout << "   +----------------------------------------------------+" << std::endl;
+    std::cout << "   |       Simulacio de la maquina ENIGMA en C++        |" << std::endl;
+    std::cout << "   |      by Marc Santacana & Abderraman Ahiayouch      |" << std::endl;
+    std::cout << "   +----------------------------------------------------+" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "   1. Xifrar missatge" << std::endl;
+    std::cout << "   2. Desxifrar missatge" << std::endl;
+    std::cout << "   3. Editar rotors" << std::endl;
+    std::cout << "   4. Sortir" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "   ------------------------------------------------------" << std::endl;
+    std::cout << "   Selecciona una opcio (1-4): ";
+
+    int opcio;
+    do {
+        std::cin >> opcio;
+        switch (opcio) {
+        case 1:
+            // xifrarMissatge();
+            break;
+        case 2:
+        // desxifrarMissatge();
+            break;
+        case 3:
+            // editarRotor();
+            break;
+        case 4:
+            std::cout << "   Sortint del programa...\n";
+            break;
+        default:
+            std::cout << " " << std::endl;
+            std::cout << "   [ERROR] Opcio invalida.\n";
+        }
+    } while (opcio != 4);
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
