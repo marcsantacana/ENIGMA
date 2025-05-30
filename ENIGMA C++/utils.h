5e7cc6fa048c6b4821ae0300e9ea14a271bfe60b
AbderrEnti <151615471+AbderrEnti@users.noreply.github.com>
2025-05-30 18:09:25 +0200

    feat: afegit neteja i format de text .h

---
+++ ENIGMA C++/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+int lletraAIndex(char c); 
+char indexALletra(int i);
+std::string netejarText(const std::string& text);
+std::string formatGrupsDe5(const std::string& text);
+
+#endif
